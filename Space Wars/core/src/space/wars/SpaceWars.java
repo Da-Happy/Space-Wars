6297cb41c254d6bb31087a0a9586f3ffba92229c
Da-Happy <70923994+Da-Happy@users.noreply.github.com>
2022-02-06 21:14:36 -0700

    Revert "Delete Space Wars directory"

---
+++ Space Wars/core/src/space/wars/SpaceWars.java
@@ -0,0 +1,48 @@
+package space.wars;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+
+public class SpaceWars extends ApplicationAdapter {
+	public static World world;
+	Box2DDebugRenderer debugRenderer;
+	OrthographicCamera camera = new OrthographicCamera();
+	public static SpriteBatch batch;
+	public SpaceShip ship;
+	public static int overheat;
+
+	public void create() {
+		debugRenderer = new Box2DDebugRenderer(true, true, false, false, true, false);
+		camera.setToOrtho(true,240, 240);
+		world = new World(new Vector2(0, 0), true);
+		batch = new SpriteBatch();
+
+		ship = new SpaceShip(60*2,60*2);
+
+	}
+
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+
+		world.step(1/60f, 6, 2);
+
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		fixedgunlol.shewt(ship);
+		batch.end();
+		if(overheat > 0){
+			overheat = overheat-1;
+		}
+		debugRenderer.render(world, camera.combined);
+
+	}
+
+	public void dispose () {
+		world.dispose();
+	}
+}
