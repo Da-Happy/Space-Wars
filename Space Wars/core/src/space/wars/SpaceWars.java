b190a0d68204e14b9128ef090c59be4714f90100
AlexLettuce <90465046+AlexLettuce@users.noreply.github.com>
2022-02-07 18:26:29 -0700

    Add files via upload

--- Space Wars/core/src/space/wars/SpaceWars.java
+++ Space Wars/core/src/space/wars/SpaceWars.java
@@ -16,7 +16,9 @@ public class SpaceWars extends ApplicationAdapter {
 	OrthographicCamera camera = new OrthographicCamera();
 	public static SpriteBatch batch;
 	public SpaceShip ship;
+	public SpaceShip ship2;
 	public static int overheat;
+	public static int overheat2;
 
 	public void create() {
 		debugRenderer = new Box2DDebugRenderer(true, true, false, false, true, false);
@@ -25,22 +27,25 @@ public class SpaceWars extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		int centerX = round(camera.viewportWidth/2);
 		int centerY = round(camera.viewportHeight/2);
-		ship = new SpaceShip(centerX,centerY);
-
+		ship = new SpaceShip(centerX+200,centerY);
+		ship2 = new SpaceShip(centerX-200, centerY);
 	}
 
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
-
 		world.step(1/60f, 6, 2);
-
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		fixedgunlol.shewt(ship);
-		moveit.move(ship);
+		fixedgunlol.shewt(ship,true);
+		moveit.move(ship,true);
+		fixedgunlol.shewt(ship2,false);
+		moveit.move(ship2,false);
 		batch.end();
 		if(overheat > 0){
 			overheat = overheat-1;
+		}
+		if(overheat2 > 0){
+			overheat2 = overheat2-1;
 		}
 		debugRenderer.render(world, camera.combined);
 
--- Space Wars/core/src/space/wars/moveit.java
+++ Space Wars/core/src/space/wars/moveit.java
@@ -5,20 +5,36 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 
 public class moveit {
-    public static void move(SpaceShip ship) {
-        Vector2 direction = new Vector2(0,-1).rotateRad(ship.body.getAngle());
-        if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
-            ship.body.applyAngularImpulse(-25,true);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)){
-            ship.body.applyAngularImpulse(25,true);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)){
-            ship.body.applyForceToCenter(direction.x*1000,direction.y*1000,true);
+    public static void move(SpaceShip ship, boolean prime) {
+        Vector2 direction = new Vector2(0, -1).rotateRad(ship.body.getAngle());
+        if(prime) {
+            if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
+                ship.body.applyAngularImpulse(-25, true);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
+                ship.body.applyAngularImpulse(25, true);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
+                ship.body.applyForceToCenter(direction.x * 1000, direction.y * 1000, true);
 
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)){
-            ship.body.applyForceToCenter(direction.x*-1000,direction.y*-1000,true);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
+                ship.body.applyForceToCenter(direction.x * -1000, direction.y * -1000, true);
+            }
+        } else{
+            if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+                ship.body.applyAngularImpulse(-25, true);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+                ship.body.applyAngularImpulse(25, true);
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+                ship.body.applyForceToCenter(direction.x * 1000, direction.y * 1000, true);
+
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+                ship.body.applyForceToCenter(direction.x * -1000, direction.y * -1000, true);
+            }
         }
     }
 
