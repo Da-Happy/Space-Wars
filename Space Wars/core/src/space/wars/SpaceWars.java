1a24de67ef32f913bcb1fc922f9a6fc2c8cf31e6
Da-Happy <70923994+Da-Happy@users.noreply.github.com>
2022-02-14 20:39:25 -0700

    Add files via upload

--- Space Wars/core/src/space/wars/SpaceWars.java
+++ Space Wars/core/src/space/wars/SpaceWars.java
@@ -7,8 +7,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import static java.lang.Math.round;
-
 
 public class SpaceWars extends ApplicationAdapter {
 	public static World world;
@@ -16,36 +14,29 @@ public class SpaceWars extends ApplicationAdapter {
 	OrthographicCamera camera = new OrthographicCamera();
 	public static SpriteBatch batch;
 	public SpaceShip ship;
-	public SpaceShip ship2;
 	public static int overheat;
-	public static int overheat2;
 
 	public void create() {
 		debugRenderer = new Box2DDebugRenderer(true, true, false, false, true, false);
-		camera.setToOrtho(true,500, 500);
+		camera.setToOrtho(true,240, 240);
 		world = new World(new Vector2(0, 0), true);
 		batch = new SpriteBatch();
-		int centerX = round(camera.viewportWidth/2);
-		int centerY = round(camera.viewportHeight/2);
-		ship = new SpaceShip(centerX+centerX/2,centerY);
-		ship2 = new SpaceShip(centerX-centerX/2, centerY);
+
+		ship = new SpaceShip(60*2,60*2);
+
 	}
 
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
+
 		world.step(1/60f, 6, 2);
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		fixedgunlol.shewt(ship,true);
-		moveit.move(ship,true);
-		fixedgunlol.shewt(ship2,false);
-		moveit.move(ship2,false);
+		fixedgunlol.shewt(ship);
 		batch.end();
 		if(overheat > 0){
 			overheat = overheat-1;
-		}
-		if(overheat2 > 0){
-			overheat2 = overheat2-1;
 		}
 		debugRenderer.render(world, camera.combined);
 
--- Space Wars/core/src/space/wars/fixedgunlol.java
+++ Space Wars/core/src/space/wars/fixedgunlol.java
@@ -5,28 +5,21 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
 
 public class fixedgunlol {
-
-    public static void shewt(SpaceShip ship,boolean prime){
+    public static void shewt(SpaceShip ship){
+        float angle = ship.body.getAngle();
         Vector2 pos = ship.railgun.pos;
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SHIFT_LEFT) && !prime) {
+        if (Gdx.input.isButtonPressed(Input.Buttons.LEFT) && SpaceWars.overheat == 0) {
 //            ammo = ammo-1;
-            Vector2 bodyPosition = ship.body.getPosition();
-            Vector2 gunPos = new Vector2(pos.x,pos.y-ship.railgun.wY*2);
-            gunPos.rotateRad(ship.body.getAngle());
-            Vector2 direction = new Vector2(0,-1).rotateRad(ship.body.getAngle());
+            SpaceWars.overheat = 10;
+            Vector2 bodyPosition =  ship.body.getPosition();
+            Vector2 gunPos = new Vector2(pos.x+ship.railgun.wX,pos.y+ship.railgun.wY);
+            gunPos.rotateRad(angle);
+            gunPos.rotate90(1);
             gunPos.add(bodyPosition);
-            Bullet bullet = new Bullet(gunPos,direction,100000000f);
+            System.out.println(gunPos);
+            Bullet bullet = new Bullet(gunPos, 100000000f);
             bullet.body.applyForceToCenter(bullet.angularVelocity,false);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SHIFT_RIGHT) && prime) {
-//            ammo = ammo-1;
-            Vector2 bodyPosition = ship.body.getPosition();
-            Vector2 gunPos = new Vector2(pos.x,pos.y-ship.railgun.wY*2);
-            gunPos.rotateRad(ship.body.getAngle());
-            Vector2 direction = new Vector2(0,-1).rotateRad(ship.body.getAngle());
-            gunPos.add(bodyPosition);
-            Bullet bullet = new Bullet(gunPos,direction,10000000);
-            bullet.body.applyForceToCenter(bullet.angularVelocity,false);
+
         }
     }
 }
