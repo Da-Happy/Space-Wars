3616abec7a0e97709a9c3ba2a42ae9a08e096304
AlexLettuce <90465046+AlexLettuce@users.noreply.github.com>
2022-02-25 09:26:43 -0700

    Add files via upload

---
+++ Space Wars/core/src/space/wars/CollisionListener.java
@@ -0,0 +1,46 @@
+package space.wars;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+public class CollisionListener implements ContactListener{
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture fa = contact.getFixtureA();
+        Fixture fb = contact.getFixtureB();
+        if(fa == null || fb == null) return;
+        if(fa.getUserData() == null || fa.getUserData() == null) return;
+        if(fa.getBody().isBullet()){
+            SpaceWars.queue.add(fa.getBody());
+            SpaceWars.bullets.remove(fa);
+        }
+        if(fb.getBody().isBullet()){
+            SpaceWars.queue.add(fb.getBody());
+            SpaceWars.bullets.remove(fb);
+        }
+        if(!fa.getBody().isBullet()){
+            SpaceShip faShip = (SpaceShip) fa.getBody().getUserData();
+            faShip.health--;
+        }
+        if(!fb.getBody().isBullet()){
+            SpaceShip fbShip = (SpaceShip) fb.getBody().getUserData();
+            fbShip.health--;
+        }
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    //dont worry about these two
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
+
---
+++ Space Wars/core/src/space/wars/SpaceWars.java
@@ -0,0 +1,84 @@
+package space.wars;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.ScreenUtils;
+import sun.jvm.hotspot.gc.shared.Space;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.round;
+
+
+public class SpaceWars extends ApplicationAdapter {
+	public static World world;
+	Box2DDebugRenderer debugRenderer;
+	OrthographicCamera camera = new OrthographicCamera();
+	private SpriteBatch batch;
+	private Texture red;
+	private Texture blue;
+	private Texture buleet;
+	public SpaceShip ship;
+	public SpaceShip ship2;
+	public int ConstantOfGravity = 20;
+	public static int overheat;
+	public static int overheat2;
+	public static ArrayList<Body> queue;
+	public static ArrayList<Bullet> bullets = new ArrayList<>();
+	public void create() {
+		queue = new ArrayList<>();
+		debugRenderer = new Box2DDebugRenderer(false, true, false, false, false, false);
+		camera.setToOrtho(true,500, 500);
+		world = new World(new Vector2(0, 0), true);
+		world.setContactListener(new CollisionListener());
+		batch = new SpriteBatch();
+		red = new Texture("red.png");
+		blue = new Texture("blue.png");
+		buleet = new Texture("bulleet1.png");
+		int centerX = round(camera.viewportWidth/2);
+		int centerY = round(camera.viewportHeight/2);
+		ship = new SpaceShip(centerX+centerX/2,centerY);
+		ship2 = new SpaceShip(centerX-centerX/2, centerY);
+	}
+
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+		world.step(1/60f, 6, 2);
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(red, ship.body.getPosition().x-8, ship.body.getPosition().y-8,red.getWidth()/2,red.getHeight()/2,red.getWidth()*2,red.getHeight()*2,1,1,ship.body.getAngle() * MathUtils.radiansToDegrees,0,0,32,32,false,true);
+		batch.draw(blue, ship2.body.getPosition().x-8, ship2.body.getPosition().y-8,blue.getWidth()/2,blue.getHeight()/2,blue.getWidth()*2,blue.getHeight()*2,1,1,ship2.body.getAngle() * MathUtils.radiansToDegrees,0,0,32,32,false,true);
+		for(int i = 0;i<bullets.size();i++){
+			batch.draw(buleet, bullets.get(i).body.getPosition().x-2,bullets.get(i).body.getPosition().y-2,buleet.getWidth()/2,buleet.getHeight()/2,buleet.getWidth()/2,buleet.getHeight()/2,1,1,0,0,0,8,8,false,true);
+		}
+		batch.end();
+		fixedgunlol.shewt(ship,true);
+		moveit.move(ship,true);
+		fixedgunlol.shewt(ship2,false);
+		moveit.move(ship2,false);
+		for (int i = 0; i < queue.size(); i++) {
+			world.destroyBody(queue.get(i));
+		}
+		queue.clear();
+		if(ship.health <= 0){
+			queue.add(ship.body);
+			ship.health = 100;
+		}
+		if(ship2.health <= 0){
+			queue.add(ship2.body);
+			ship2.health = 100;
+		}
+		debugRenderer.render(world, camera.combined);
+	}
+
+	public void dispose () {
+
+		world.dispose();
+		batch.dispose();
+	}
+}
